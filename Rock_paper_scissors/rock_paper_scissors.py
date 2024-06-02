905d356aa4f830bd119808c4d72feb38f3bdad60
avvileiskyi <a.v.vileiskyi@student.khai.edu>
2024-06-02 20:56:12 +0300

    Update rock_paper_scissors.py

--- Rock_paper_scissors/rock_paper_scissors.py
+++ Rock_paper_scissors/rock_paper_scissors.py
@@ -1,16 +1,8 @@
 """Rock Paper Scissors module"""
 from random import choice
 
-
-def get_user_rating(username):
-    """Gets the user's rating from the 'rating' file if it's possible
-
-    Args:
-        username (str): user's name
-
-    Returns:
-        int: user's rating points
-    """
+def get_rating(username):
+    """Get user's rating from file."""
     with open("rating", "r", encoding="utf-8") as file:
         for line in file:
             name, points = line.split()
@@ -18,89 +10,57 @@ def get_user_rating(username):
                 return int(points)
     return 0
 
+def play_round(user_choice, game_rules):
+    """Play a round of Rock Paper Scissors."""
+    computer_choice = choice(list(game_rules.keys()))
 
-def start_game(user_option, rules):
-    """Starts a round of the Rock Paper Scissors game
-
-    Args:
-        user_option (str): user's choice
-        rules (dict): dictionary with game rules
-
-    Returns:
-        int: points earned in the round
-    """
-    computer_option = choice(list(rules.keys()))
-
-    if computer_option in rules[user_option]:
-        print(f"Well done. The computer chose {computer_option} and failed")
+    if computer_choice in game_rules[user_choice]:
+        print(f"Well done. The computer chose {computer_choice} and failed")
         return 100
-    elif user_option == computer_option:
-        print(f"There is a draw ({user_option})")
+    elif user_choice == computer_choice:
+        print(f"There is a draw ({user_choice})")
         return 50
     else:
-        print(f"Sorry, but the computer chose {computer_option}")
+        print(f"Sorry, but the computer chose {computer_choice}")
         return 0
 
+def display_rules(game_rules):
+    """Display the game rules."""
+    for option in game_rules:
+        print(f"{option} can beat: {', '.join(game_rules[option])}")
 
-def print_rules(rules):
-    """Prints the game rules
-
-    Args:
-        rules (dict): dictionary with game rules
-    """
-    for option in rules:
-        print(f"{option} can beat: {', '.join(rules[option])}")
-
-
-def get_user_rules(options):
-    """Gets user-defined rules from a list of options
-
-    Args:
-        options (list): list of user-defined options
-
-    Returns:
-        dict: user-defined game rules
-    """
+def generate_rules(options):
+    """Generate user-defined rules."""
     rules = {}
-
     for i in range(len(options)):
-        rules[options[i]] = [options[i] for i in range(i-len(options)//2, i)]
-
+        rules[options[i]] = [options[j % len(options)] for j in range(i + 1, i + len(options) // 2 + 1)]
     return rules
 
-
 def start_menu():
-    """Start the main menu of the Rock Paper Scissors game"""
-
+    """Start the main menu."""
     name = input("Enter your name: > ")
     print(f"Hello, {name}")
 
     options = input("Enter the options in format \"rock,paper,scissors\": > ")
     print("Okay, let's start")
 
-    user_rating = get_user_rating(name)
+    user_rating = get_rating(name)
     default_rules = {"rock": ["scissors"], "paper": ["rock"], "scissors": ["paper"]}
-    rules = default_rules if options == '' else get_user_rules(options.split(","))
+    game_rules = default_rules if options == '' else generate_rules(options.split(","))
 
     while True:
-        user_choice = input("> ")
-        if user_choice == "!exit":
+        user_input = input("> ")
+        if user_input == "!exit":
             print("Bye!")
             break
-
-        elif user_choice == "!rules":
-            print_rules(rules)
-
-        elif user_choice == "!rating":
+        elif user_input == "!rules":
+            display_rules(game_rules)
+        elif user_input == "!rating":
             print(f"Your rating is {user_rating}!")
-
-        elif user_choice in rules.keys():
-            user_rating += start_game(user_choice, rules)
-
+        elif user_input in game_rules.keys():
+            user_rating += play_round(user_input, game_rules)
         else:
             print("Incorrect input.")
 
-
 if __name__ == "__main__":
     start_menu()
-
